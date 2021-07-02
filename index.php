3651cb98d617985633ace2ae1592c21e58dc3dda
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2021-07-01 22:01:36 -0700

    New Logo Update

--- index.php
+++ index.php
@@ -32,25 +32,34 @@ file_put_contents('visitors.log', $ip_address . PHP_EOL, FILE_APPEND);
 
  
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Base</title>
     <link rel="stylesheet" href="styles.css">
-    </head>
-    <body onload="startTime()">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+</head>
+
+<body onload="startTime()">
     <button class="button">Login</button>
     <br><br>
-    <h1 class="test1">Hacking Team Base</h1>
-    <h1>Welcome To The Hacking Team Base.</h1>
-<h3><b>This is only for dedicated hackers. Made By K and Z</b></h3>
-<h4>Message hexus#2444 on discord to join.<h4>
-    <div id='clock'></div>
     <br>
+    <img src="path21383.png" alt="Hacking Empire's Logo">
+    <h1 class="test1">Hacking Team Base</h1>
+
+    <h1>Welcome to the Team Base for Hacking Empire.</h1>
+    <h3><b>This is only for dedicated hackers. Made By K and Z</b></h3>
+    <h4>Message hexus#2444 on discord to join.<h4>
+            <div id='clock'></div>
+            <br>
 
 
 
@@ -64,8 +73,9 @@ file_put_contents('visitors.log', $ip_address . PHP_EOL, FILE_APPEND);
 
 
 
-<script src="script.js"></script>
+            <script src="script.js"></script>
 </body>
+
 </html>
 <?php
 
