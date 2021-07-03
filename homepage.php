419da12f69f85274d3df3262c8902e4360f48a5a
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2021-07-02 19:25:43 -0700

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,23 @@
+<?php
+   ob_start();
+   session_start();
+?>
+
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
+
+<html lang="en">
+
+<head>
+    <title>Login</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+</body>
+
+</html>
