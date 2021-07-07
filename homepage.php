d035c4d35c8a1d8e91e9b6b216fb8804ae5f8a30
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2021-07-06 22:31:46 -0700

    404 update

--- homepage.php
+++ homepage.php
@@ -18,11 +18,13 @@ exit();
 <html lang="en">
 
 <head>
-    <title>Login</title>
+    <title>Home</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <?php
     $css_path = '/styles.css';
-
+if($_SESSION['username'] == 'Zakariah'){
+   $css_path = '/404.css';
+}
     ?>
     <link rel="stylesheet" type="text/css" 
 href="<?php echo $css_path;?>">
@@ -37,6 +39,47 @@ echo $greet;
 
 
 ?>
+<link rel="stylesheet" href="/404.css">
+<div class="moon"></div>
+<div class="moon__crater moon__crater1"></div>
+<div class="moon__crater moon__crater2"></div>
+<div class="moon__crater moon__crater3"></div>
+
+<div class="star star1"></div>
+<div class="star star2"></div>
+<div class="star star3"></div>
+<div class="star star4"></div>
+<div class="star star5"></div>
+
+
+<div class="astronaut">
+  <div class="astronaut__backpack"></div>
+  <div class="astronaut__body"></div>
+  <div class="astronaut__body__chest"></div>
+  <div class="astronaut__arm-left1"></div>
+  <div class="astronaut__arm-left2"></div>
+  <div class="astronaut__arm-right1"></div>
+  <div class="astronaut__arm-right2"></div>
+  <div class="astronaut__arm-thumb-left"></div>
+  <div class="astronaut__arm-thumb-right"></div>
+  <div class="astronaut__leg-left"></div>
+  <div class="astronaut__leg-right"></div>
+  <div class="astronaut__foot-left"></div>
+  <div class="astronaut__foot-right"></div>
+  <div class="astronaut__wrist-left"></div>
+  <div class="astronaut__wrist-right"></div>
+  
+  <div class="astronaut__cord">
+    <canvas id="cord" height="500px" width="500px"></canvas>
+  </div>
+  
+  <div class="astronaut__head">
+    <canvas id="visor" width="60px" height="60px"></canvas>
+    <div class="astronaut__head-visor-flare1"></div>
+    <div class="astronaut__head-visor-flare2"></div>
+  </div>
+</div>
+<script src="/4042.js"></script>
 </body>
 
 </html>
